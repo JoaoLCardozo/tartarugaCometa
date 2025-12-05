5a4af5d7a8ff720e38831e236112b128b7eddb93
JoaoLCardozo <jolucardozo@gmail.com>
2025-12-05 09:01:03 -0300

    First Commit

--- src/main/java/br/com/tartarugaCometa/dao/ClienteDAO.java
+++ src/main/java/br/com/tartarugaCometa/dao/ClienteDAO.java
@@ -14,17 +14,21 @@ public class ClienteDAO {
 
     public void salvarCliente(Cliente cliente) {
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(INSERT_SQL)) {
-
+        		 
+             PreparedStatement ps  = conn.prepareStatement(INSERT_SQL)) {
+        	
             ps.setString(1, cliente.getNomeRazao());
             ps.setString(2, cliente.getDocumento());
             ps.setString(3, cliente.getTpDocumento());
             ps.setInt(4, cliente.getEndereco().getIdEndereco());
-
             ps.executeUpdate();
             System.out.println("Cliente salvo com sucesso.");
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao salvar cliente", e);
         }
     }
+    
+    
+    
+    
 }
